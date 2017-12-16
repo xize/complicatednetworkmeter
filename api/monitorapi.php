dc9709d6daf08c67033705d50a26d008843205b6
xize <glucassen@live.nl>
2017-12-16 20:14:25 +0100

    seperate code from classes for better visibillity

---
+++ api/monitorapi.php
@@ -0,0 +1,27 @@
+<?php
+require_once("monitordatabase.php");
+class MonitorAPI extends MonitorDatabase {
+
+	private $servicename;
+	private $dns;
+	private $pingip;
+
+	public function __construct($data) {
+		$this->servicename = $data[0];
+		$this->dns = ($data[1] == "up" ? true : false);
+		$this->pingip = ($data[2] == "up" ? true : false);
+	}
+
+	public function getName() {
+		return $this->servicename;
+	}
+	
+	public function isDNSActive() {
+		return $this->dns;
+	}
+	
+	public function isPINGActive() {
+		return $this->pingip;
+	}
+	
+}
