be4e17330fd4b1193c3b06a55ad5b0cbf3f685d0
xize <glucassen@live.nl>
2017-12-17 22:17:24 +0100

    add some buggy code

--- index.php
+++ index.php
@@ -14,12 +14,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 use complicatednetworkmeter\api;
 
 namespace complicatednetworkmeter {
 	if(!file_exists("config.php")) {
-		header("Location: /install/");
+		header ("Location: install/index.php");
 	} else {
 		$cfg = new Config();
 		if($cfg->IsMonitor()) {
