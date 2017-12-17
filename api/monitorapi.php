11ca2597e8239ef1cd0fcdff8b9685dfc5374b0e
xize <glucassen@live.nl>
2017-12-17 15:20:57 +0100

    add some documentation to the classes

--- api/monitorapi.php
+++ api/monitorapi.php
@@ -23,20 +23,45 @@ namespace complicatednetworkmeter\api {
 		private $dns;
 		private $pingip;
 
+		/**
+		* constructs a new MonitorAPI class
+		*
+		* @author xize
+		* @param data - a array where 0 is the service name, 1 contains a string when the dns is up or false, 2 contains a string when the ping command has been succeeded or failed.
+		*/
 		public function __construct($data) {
 			$this->servicename = $data[0];
 			$this->dns = ($data[1] == "up" ? true : false);
 			$this->pingip = ($data[2] == "up" ? true : false);
 		}
 	
+
+		/**
+		* gets the name of the service we are reading
+		*
+		* @author xize
+		* @return string
+		*/
 		public function getName() {
 			return $this->servicename;
 		}
 	
+		/**
+		* returns true if the dns is active false otherwise
+		*
+		* @author xize
+		* @return bool
+		*/
 		public function isDNSActive() {
 			return $this->dns;
 		}
 	
+		/**
+		* returns true if the ping request succeeded, otherwise false
+		*
+		* @author xize
+		* @return bool
+		*/
 		public function isPINGActive() {
 			return $this->pingip;
 		}
