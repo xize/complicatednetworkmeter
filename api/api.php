9f80065f433ebc1c932be507478d603164609eb8
xize <glucassen@live.nl>
2017-12-20 14:28:04 +0100

    remove useless 'use' keyword on the same namespace

--- api/api.php
+++ api/api.php
@@ -15,8 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-use complicatednetworkmeter;
-
 namespace complicatednetworkmeter {
 
 	$cfg = new Config();
