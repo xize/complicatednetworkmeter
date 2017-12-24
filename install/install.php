e506e2c859909b1d5f1a62803a7af6988ac6dcaa
xize <glucassen@live.nl>
2017-12-24 14:42:45 +0100

    do not enforce passwords on databases

--- install/install.php
+++ install/install.php
@@ -60,7 +60,7 @@ namespace complicatednetworkmeter\install {
                         echo "</form>";
                     break;
                     case "2":
-                        if(isset($_POST['dbuser']) && strlen($_POST['dbuser']) > 0 && isset($_POST['dbpasswd']) && strlen($_POST['dbpasswd']) > 0 && isset($_POST['dbname']) && strlen($_POST['dbname']) > 0) {
+                        if(isset($_POST['dbuser']) && strlen($_POST['dbuser']) > 0 && isset($_POST['dbname']) && strlen($_POST['dbname']) > 0) {
                             echo "<h3>please check if the database connection succeeded!</h3>";
                             echo "<hr>";
                             echo "testing connection...";
@@ -138,7 +138,7 @@ namespace complicatednetworkmeter\install {
         * @author xize
         */
         public function createDatabase($network, $user, $password, $db) {
-            $sql = new mysqli($network, $user, $password, $db);
+            $sql = new Mysqli($network, $user, $password, $db);
             $stmt = $sql->prepare("CREATE DATABASE IF NOT EXISTS " . $db . "");
             $stmt->execute();
             if(!mysql_errno()) {
@@ -153,7 +153,7 @@ namespace complicatednetworkmeter\install {
         * @author xize
         */
         public function addTables($network, $user, $password, $db) {
-            $sql = new mysqli($network, $user, $password, $db);
+            $sql = new Mysqli($network, $user, $password, $db);
             $stmt = $sql->prepare("
                 CREATE TABLE IF NOT EXISTS `Monitor` (
                     `id` int(254) NOT NULL AUTO_INCREMENT,
