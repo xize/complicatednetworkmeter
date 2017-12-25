d2dd6d6f5c712fbaeadc2551ac233bfaa56c8e0f
xize <glucassen@live.nl>
2017-12-26 00:20:54 +0100

    add alot of fixes towards the install script

--- install/install.php
+++ install/install.php
@@ -17,14 +17,13 @@ limitations under the License.
 
 namespace complicatednetworkmeter\install {
 
+    session_start();
+
     require_once("license.php");
     require_once("salt.php");
 
     class Install {
 
-        private $duser;
-        private $dpass;
-        private $db;
 
         /**
         * returns the page routing and content for the installscript.
@@ -32,6 +31,7 @@ namespace complicatednetworkmeter\install {
         * @author xize
         */
         public function getContent() {
+        
             echo "<div class=\"content\"/>";
             if(!isset($_GET['step'])) {
                 if(isset($_COOKIE['agreed'])) {
@@ -66,8 +66,13 @@ namespace complicatednetworkmeter\install {
                             echo "<hr>";
                             echo "<p>testing connection...</p>";
                             $con = $this->testConnection("localhost", $_POST['dbuser'], $_POST['dbpasswd'], $_POST['dbname']);
+                            
                             if($con) {
-                                setcookie("sqlsucceed", 1, +3600);
+                                $_SESSION['network'] = "localhost";
+                                $_SESSION['user'] = $_POST['dbuser'];
+                                $_SESSION['pass'] = $_POST['dbpasswd'];
+                                $_SESSION['db'] = $_POST['dbname'];
+                                
                                 //create database...
                                 echo $con ? "<p style=\"color:green\">[+] the connection was successfull</p>" : "<p style=\"color:red\">[x] the connection failed!</p>";
                                 echo "<p>creating database now...</p>";
@@ -77,42 +82,53 @@ namespace complicatednetworkmeter\install {
                                         echo "<p style=\"color:green\">[+] tables are successfully created!</p>";
                                     } else {
                                         echo "<p style=\"color:red\">[x] failed to add tables!</p>";
-                                        unset($_COOKIE['sqlsucceed']);
+                                        unset($_SESSION);
+                                        session_destroy();
                                     }
                                 } else {
                                     echo "<p style=\"color:red\">[x] failed to create database!</p>";
-                                    unset($_COOKIE['sqlsucceed']);
+                                    unset($_SESSION);
+                                    session_destroy();
                                 }
                                 //end creating database
-
-                                //no need to use sessions here we store the values in local variables.
-                                $this->duser = $_POST['dbuser'];
-                                $this->dpass = $_POST['dbpasswd'];
-                                $this->db = $_POST['dbname'];
                             } else {
                                 echo "<p style=\"color:red\">[x] unable to connect to the database!, please fill in your settings again</p>";
-                                unset($_COOKIE['sqlsucceed']);
                             }
-                            echo "<p><button onclick=\"window.location.href='?step=1'\">back</button>". (isset($_COOKIE['sqlsucceed']) ? "<button onclick=\"window.location.href='?step=3'\"/>next</button>" : "")."</p>";
+                            echo "<p><button onclick=\"window.location.href='?step=1'\">back</button><button onclick=\"window.location.href='?step=3'\"/>next</button></p>";
                         } else {
                             $this->showError("one of the forms was empty or not filled in!", "you get redirected back to the previous page in 10 seconds!");
                             header("refresh:10;URL=?step=1");
                         }
                     break;
                     case "3":
-                        if(!isset($_COOKIE['sqlsucceed'])) {
-                            $this->showError("no cookies found!", "you get redirected back to the previous page in 10 seconds!");
+                        if(!isset($_SESSION['user'])) {
+                            $this->showError("no active session found!", "you get redirected back to the previous page in 10 seconds!");
                             header("refresh: 10;URL=index.php");
-                            return;
+                           return;
                         }
                         echo "<h3>please create a administrator account!</h3>";
                         echo "<hr>";
-                        echo "<form name=\"account\" method=\"post\"/>";
-                        echo "  <p>username: <input type=\"text\" name=\"user\" value=\"username\"/></p>";
-                        echo "  <p>password: <input type=\"password\" name=\"password\" value=\"password\"/></p>";
+                        echo "<form name=\"account\" method=\"post\" action=\"?step=4\"/>";
+                        echo "  <p>username: <input type=\"text\" name=\"cms_user\" value=\"username\"/></p>";
+                        echo "  <p>password: <input type=\"password\" name=\"cms_password\" value=\"password\"/></p>";
                         echo "  <p>re-type password: <input type=\"password\" name=\"password2\" value=\"password\"/></p>";
-                        echo "</form>";
-                        echo "<p><button onclick=\"window.location.href='?step=2'\">back</button><button onclick=\"window.location.href='?step=4'\">next</button></p>";
+                        echo "<p><button onclick=\"window.location.href='?step=2'\">back</button><button type=\"submit\" onclick=\"window.location.href='?step=4'\">next</button></p></form>";
+                    break;
+                    case "4":
+                        if(isset($_POST['cms_user']) && isset($_POST['cms_password'])) {
+                            if($_POST['cms_password'] != $_POST['password2']) {
+                                showError("passwords did not match!", "you get redirected to the previous page over 5 seconds!");
+                                header("refresh:5;URL=?step=3");
+                                return;
+                            }
+                            echo "<h3>successfully created login information!</h3>";
+                            echo "<hr>";
+                            echo "<p>please click <a href=\"../admin.php\"/>here</a> to login to your admin panel!</p>";
+                            $this->createUser($_SESSION['network'], $_SESSION['user'], $_SESSION['pass'], $_SESSION['db'], $_POST['cms_user'], $_POST['cms_password']);
+                        } else {
+                            showError("failed to create user empty forms!", "we redirect you to the previous page in 5 seconds!");
+                            header("refresh:5;URL=?step=3");
+                        }
                     break;
                     default:
                         header("Location: /index.php");
@@ -162,6 +178,7 @@ namespace complicatednetworkmeter\install {
                     `dns`  varchar(8) NOT NULL,
                     `ping` varchar(8) NOT NULL,
                     PRIMARY KEY(`id`),
+                    UNIQUE `monitor` (name),
                     KEY `name` (`name`)
                 )");
             $stmt2 = $sql->prepare("
@@ -170,31 +187,23 @@ namespace complicatednetworkmeter\install {
                     `name` varchar(100) NOT NULL,
                     `password`  varchar(100) NOT NULL,
                     PRIMARY KEY(`id`),
+                    UNIQUE `users` (name),
                     KEY `name` (`name`)
                 )");
             return $stmt->execute() && $stmt2->execute();
         }
 
-        public function createUser($user, $password) {
-            $sql = new \mysqli("localhost", $this->duser, $this->dpass, $this->db);
-            $stmt = $sql->prepare("INSERT INTO users(name, password) VALUES(?, ?)");
-            
-            //first lets salt the password to somehting magic and uncrackable.
-            $salted = Salt::getGenerator()->salt($password, 2048);
-
-            ///now lets encrypt it by AES-256-CBC
-
-            $ivlength = openssl_cipher_iv_length($cipher="AES-256-CBC");
-            $iv = openssl_random_pseudo_bytes($ivlength);
-            $encrypted = openssl_encrypt($salted, $cipher, $options=OPENSSL_RAW_DATA, $iv);
-            $hmac = hash_hmac('sha256', $encrypted, true);
-            
-            //now compress the encrypted password by base64
-
-            $finalpass = base64_encode($iv.$hmac.$encrypted);
-
-            $stmt->bind("ss", $user, $finalpass);
-            var_dump($stmt->execute());
+        /**
+        * creates the user with a strong encrypted password based on AES-256-CBC and salted.
+        *
+        * @author xize
+        */
+        public function createUser($network, $user, $password, $db, $cms_user, $cms_password) {
+            $sql = new \mysqli($network, $user, $password, $db);
+            $stmt = $sql->prepare("INSERT INTO users (name, password) VALUES (?, ?)");
+            $finalpass = $this->encrypt($cms_password);
+            $stmt->bind_param("ss", $cms_user, $finalpass);
+            $stmt->execute();
         }
 
         /**
@@ -206,6 +215,21 @@ namespace complicatednetworkmeter\install {
             echo "<h3>error: ". $title ."</h3>";
             echo "<hr>";
             echo "<p class=\"error\">" . $text . "</p>";
+        }
+
+        /**
+        * encrypts the password into something considered to be 'uncrackable'
+        *
+        * @author xize
+        */
+        public function encrypt($password) {
+            $salted = $salted = \Salt::getGenerator()->createSalt($password, 2048);
+            $ivlength = openssl_cipher_iv_length($cipher="AES-256-CBC");
+            $iv = openssl_random_pseudo_bytes($ivlength);
+            $encrypted = openssl_encrypt($salted, $cipher, $options=OPENSSL_RAW_DATA, $iv);
+            $hmac = hash_hmac('sha256', $encrypted, true);
+            $finalpass = base64_encode($hmac.$encrypted);
+            return $finalpass;
         }
     }
 }
