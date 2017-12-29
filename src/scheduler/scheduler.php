a27d33974351938c1a026da7c24d558c831ede7b
xize <glucassen@live.nl>
2017-12-29 18:50:53 +0100

    add basic scheduler (not sure if it works)

---
+++ src/scheduler/scheduler.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Copyright 2017 Guido Lucassen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+namespace complicatednetworkmeter\scheduler {
+
+    abstract class Scheduler { 
+
+        private $isrunning = false;
+        private $ticks = 0;
+
+        /**
+        * starts the scheduler
+        *
+        * @param ticks - how long it takes before the next tick
+        * @author xize
+        */
+        public function start(int $ticks) {
+            $this->ticks = $ticks;
+        }
+
+        /**
+        * returns the tick rate of this scheduler
+        *
+        * @author xize
+        */
+        public function getTick() {
+            return $this->ticks;
+        }
+
+        /**
+        * returns true if the scheduler is running
+        *
+        * @author xize
+        */
+        public function isRunning() {
+            return $this->isrunning;
+        }
+
+        /**
+        * stops the scheduler
+        *
+        * @author xize
+        */
+        public function stop() {
+            $this->isrunning = false;
+        }
+
+        /**
+        * the task inside this scheduler
+        *
+        * @author xize
+        */
+        public abstract function run();
+
+    }
+
+}
---
+++ src/scheduler/task.php
@@ -0,0 +1,46 @@
+<?php
+<?php
+/*
+Copyright 2017 Guido Lucassen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+namespace complicatednetworkmeter\scheduler {
+    
+    require_once("scheduler.php");
+
+    class Task extends Scheduler {
+        
+        private $f;
+
+        public function __construct($f, $ticks) {
+            $this->f = f;
+        }
+
+        public function run() {
+            while($this->isRunning()) {
+                sleep($this->getTick());
+                $f();
+            }
+        }
+    }
+
+    $i = 0;
+
+    #test...
+    $task = new Task(function() {
+        echo "hello world!, this is my ".$i++." visit!";
+    }).start(10);
+
+}
