35b8c4b81ae051298bee7a83aed3b7e75a2c06ba
xize <glucassen@live.nl>
2017-12-31 17:40:14 +0100

    add test folder, and updated sync

---
+++ test/index.php
@@ -0,0 +1,20 @@
+<?php
+class Test {
+
+    public function foo() {
+        $this->say();
+    }
+
+    public function say() {
+        for($i = 0; $i < 16;$i++) {
+            echo "<p>hello</p>";
+        }
+    }
+}
+
+set_time_limit(0);
+
+$testing = new Test();
+
+declare(ticks = 1);
+register_tick_function([&$testing, 'foo'], true);
