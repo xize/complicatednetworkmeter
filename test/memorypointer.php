5db6ef8dd8c31f7c739e67d157450ca0751f0ffd
xize <glucassen@live.nl>
2017-12-31 20:43:32 +0100

    add another test with memory pointer (no success)

--- test/memorypointer.php
+++ test/memorypointer.php
@@ -2,44 +2,50 @@
 class Bad {
     
     private $array;
+    private $sword1;
+    private $sword2;
 
     public function __construct() {
         $this->array = array();
+        $this->sword1 = new SpecialMagicalSword(1, "KYLE");
+        $this->sword2 = new SpecialMagicalSword(10, "ALDUIN");
     }
 
-    public function setInArray(SpecialMagicalSword $sword1, SpecialMagicalSword $sword2) {
-        $this->array[] = $sword1;
-        $this->array[] = $sword2;
+    public function getSword1() {
+        return $this->sword1;
     }
 
-    public function showContents() {
-        foreach($this->array as $sword) {
-            echo "<p>".$sword->getName()."</p>";
-            echo "<p>".$sword->getDMG()."</p>";
-            echo "</br>";
-        }
+    public function getSword2() {
+        return $this->sword2;
+    }
+
+    public function setSwordDamage(SpecialMagicalSword $sword, int $dmg) {
+        $sword->setDMG($dmg);
     }
 
 }
 
 class Good {
     private $array;
+    private $sword1;
+    private $sword2;
 
     public function __construct() {
         $this->array = array();
+        $this->sword1 = new SpecialMagicalSword(1, "KYLE");
+        $this->sword2 = new SpecialMagicalSword(10, "ALDUIN");
     }
 
-    public function setInArray(SpecialMagicalSword &$sword1, SpecialMagicalSword &$sword2) {
-        $this->array[] = $sword1;
-        $this->array[] = $sword2;
+    public function getSword1() {
+        return $this->sword1;
     }
 
-    public function showContents() {
-        foreach($this->array as $sword) {
-            echo "<p>".$sword->getName()."</p>";
-            echo "<p>".$sword->getDMG()."</p>";
-            echo "</br>";
-        }
+    public function getSword2() {
+        return $this->sword2;
+    }
+
+    public function setSwordDamage(SpecialMagicalSword &$sword, int $dmg) {
+        $sword->setDMG($dmg);
     }
 }
 
@@ -60,12 +66,28 @@ class SpecialMagicalSword {
     public function getDMG() {
         return $this->sworddmg;
     }
+
+    public function setDMG(int $dmg) {
+        $this->sworddmg = $dmg;
+    }
 }
 
 $bad = new Bad();
-$bad->setInArray(new SpecialMagicalSword(10, "ALDUIN"), new SpecialMagicalSword(1, "KYLE"));
-$bad->showContents();
+echo "<p>[bad sword]</p>";
+echo "<p>default sword dmg: ".$bad->getSword1()->getDMG()."</p>";
+echo "<p>... now we change the damage of this sword! (*wooo!* magic sounds appear) ...</p>";
+
+$bad->setSwordDamage($bad->getSword1(), 100);
+
+echo "<p>the new damage of this sword is: ". $bad->getSword1()->getDMG() ."</p>";
+echo "</br>";
 
 $good = new Good();
-$good->setInArray(new SpecialMagicalSword(10, "ALDUIN"), new SpecialMagicalSword(1, "KYLE"));
-$good->showContents();
+echo "<p>[good sword]</p>";
+echo "<p>default sword dmg: ".$good->getSword1()->getDMG()."</p>";
+echo "<p>... now we change the damage of this sword! (*wooo!* magic sounds appear) ...</p>";
+
+$good->setSwordDamage($good->getSword1(), 100);
+
+echo "<p>the new damage of this sword is: ". $good->getSword1()->getDMG() ."</p>";
+echo "</br>";
