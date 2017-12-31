48eb59119d449e78f7c5a86a91c50d24ef8efa3c
xize <glucassen@live.nl>
2017-12-31 18:27:31 +0100

    add test list index page

--- test/index.php
+++ test/index.php
@@ -1,22 +1,28 @@
 <?php
-class Test {
-
-    public function foo() {
-        $this->say();
-    }
-
-    public function say() {
-        for($i = 0; $i < 16;$i++) {
-            echo "<p>hello</p>";
+if(isset($_GET['test']) && strpos($_GET['test'], "..") === false && strpos($_GET['test'], "./") === false && strpos($_GET['test'], ".\\") === false) {
+    include($_GET['test'].".php");
+} else {
+    
+    $files = glob("*.php");
+    
+    echo "<!DOCTYPE html>";
+    echo "<head>";
+    echo "  <title>test environment!</title>";
+    echo "  <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>";
+    echo "</head>";
+    echo "<body>";
+    echo "  <h1>testing environment:</h1>";
+    echo "  <p>make sure you never include these inside the CMS itself!</p>";
+    echo "  <hr>";
+    echo "  <h4>tests:</h4>";
+    echo "  <ul>";
+    foreach($files as $file) {
+        if(strpos($file, "index") === false) {
+            $lindex = strrpos($file, ".php");
+            echo "<li><a href=\"?test=".substr($file, 0, $lindex)."\"/>".substr($file, 0, $lindex)."</a></li>";
         }
     }
+    echo "  </ul>";
+    echo "</body>";
+    echo "</html>";
 }
-
-set_time_limit(0);
-
-$testing = new Test();
-
-declare(ticks = 1);
-
-//this for some reason timeouts the thread
-register_tick_function([&$testing, 'foo'], true);
